446c60246b8fa4b235b1eea0b304d6f7e4020ef1
Tatsuya Takamura <takamura@tonchidot.com>
2015-07-05 16:05:37 +0900

    [from now] Sun Jul  5 16:05:37 JST 2015

--- caffe/deepdream.py
+++ caffe/deepdream.py
@@ -90,5 +90,5 @@ net = caffe.Classifier('tmp.prototxt', param_fn,
                        mean = np.float32([104.0, 116.0, 122.0]), # ImageNet mean, training set dependent
                        channel_swap = (2,1,0))                   # the reference model has channels in BGR order instead of RGB
 
-img = np.float32(PIL.Image.open('sky1024px.jpg'))
+img = np.float32(PIL.Image.open('cloud.jpg'))
 showarray(img)
