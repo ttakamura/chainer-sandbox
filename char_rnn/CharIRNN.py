edc5a109d576d126a4167686f5f3eb9c31059c58
Tatsuya Takamura <takamura@tonchidot.com>
2015-07-18 21:49:52 +0900

    [from now] Sat Jul 18 21:49:52 JST 2015

--- char_rnn/CharIRNN.py
+++ char_rnn/CharIRNN.py
@@ -15,35 +15,34 @@ class CharIRNN(FunctionSet):
         for param in self.parameters:
             param[:] = np.random.uniform(-0.08, 0.08, param.shape)
         # IRNN
-        self.l1_h.W = np.eye(self.l1_h.W.shape[0], dtype=np.float32) * 0.4
-        self.l2_h.W = np.eye(self.l2_h.W.shape[0], dtype=np.float32) * 0.4
+        self.l1_h.W = np.eye(self.l1_h.W.shape[0], dtype=np.float32) * 0.5
+        self.l2_h.W = np.eye(self.l2_h.W.shape[0], dtype=np.float32) * 0.5
 
     def forward_one_step(self, x_data, y_data, state, train=True, dropout_ratio=0.5):
-        x = Variable(x_data, volatile=not train)
-        t = Variable(y_data, volatile=not train)
+        x  = Variable(x_data, volatile=not train)
+        t  = Variable(y_data, volatile=not train)
+        h0 = self.embed(x)
+        if dropout_ratio > 0.1:
+            h0 = F.dropout(h0, ratio=dropout_ratio, train=train)
 
-        h0      = self.embed(x)
-        h1_in   = self.l1_x(F.dropout(h0, ratio=dropout_ratio, train=train)) + self.l1_h(state['h1'])
-        h1      = F.leaky_relu(h1_in)
-        h2_in   = self.l2_x(F.dropout(h1, ratio=dropout_ratio, train=train)) + self.l2_h(state['h2'])
-        h2      = F.leaky_relu(h2_in)
-        y       = self.l3(F.dropout(h2, ratio=dropout_ratio, train=train))
-        state   = {'h1': h1, 'h2': h2}
+        h1 = F.leaky_relu(self.l1_x(h0) + self.l1_h(state['h1']))
+        if dropout_ratio > 0.1:
+            h1 = F.dropout(h1, ratio=dropout_ratio, train=train)
 
-        return state, F.softmax_cross_entropy(y, t)
+        h2 = F.leaky_relu(self.l2_x(h1) + self.l2_h(state['h2']))
+        if dropout_ratio > 0.1:
+            h2 = F.dropout(h2, ratio=dropout_ratio, train=train)
+
+        y  = self.l3(h2)
+        return {'h1': h1, 'h2': h2}, F.softmax_cross_entropy(y, t)
 
     def predict(self, x_data, state):
-        x = Variable(x_data, volatile=True)
-
-        h0      = self.embed(x)
-        h1_in   = self.l1_x(h0) + self.l1_h(state['h1'])
-        h1      = F.leaky_relu(h1_in)
-        h2_in   = self.l2_x(h1) + self.l2_h(state['h2'])
-        h2      = F.leaky_relu(h2_in)
-        y       = self.l3(h2)
-        state   = {'h1': h1, 'h2': h2}
-
-        return state, F.softmax(y)
+        x  = Variable(x_data, volatile=True)
+        h0 = self.embed(x)
+        h1 = F.leaky_relu(self.l1_x(h0) + self.l1_h(state['h1']))
+        h2 = F.leaky_relu(self.l2_x(h1) + self.l2_h(state['h2']))
+        y  = self.l3(h2)
+        return {'h1': h1, 'h2': h2}, F.softmax(y)
 
     def make_initial_state(self, n_units, batchsize=50, train=True):
         return {name: Variable(np.zeros((batchsize, n_units), dtype=np.float32),
